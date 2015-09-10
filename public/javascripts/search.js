bd1482069df3260d21af315813107b92bbd4a18b
apradhn <abhijit.pradhan6@gmail.com>
2015-09-10 17:50:29 -0400

    adds user's tracks to the page when a user's name is clicked

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,7 +2,7 @@ $(function() {
   SC.initialize({
     client_id: '81eaf8f6864fc6b8e13419c0628918d6'
   });  
-  
+
   $('#search').click(function() {
     var query = $('input[name="query"]').val();
     var searchResults = $('.search-results');
@@ -11,16 +11,30 @@ $(function() {
       console.log(users);
       list = '<ul>';
       users.forEach(function(user) {
-        list += '<li>';
-        list += '<a href="#" class="user" data-user-id="' + user.id + '">';
+        list += '<li class="user" data-user-id="' + user.id + '">';
         list += user.full_name;
-        list += '</a>';
         list += '</li>';
       });
       list += "</ul>";
-      searchResults.append(list);
+      searchResults.children('ul').replaceWith(list);
     });
   });
 
+  $(document).on('click', '.user', function() {
+    var userId = $(this).data('user-id');
+    var path = '/users/' + userId + '/tracks';
+    var html = '';
+    SC.get(path, function(tracks) {
+      html += '<ul>';
+      tracks = tracks.slice(0, 6);
+      tracks.forEach(function(track) {
+        html += '<li>';
+        html += track.title;
+        html += '</li>';
+      });
+      html += '</ul>';
+      $('.tracks').html(html);
+    });
+  });
 
 });
