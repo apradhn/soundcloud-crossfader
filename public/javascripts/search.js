1f9a18fdbaebece69bac3ee1aaa81a85f3b8267f
apradhn <abhijit.pradhan6@gmail.com>
2015-09-10 19:22:31 -0400

    updates widget embedding logic

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -12,9 +12,10 @@ $(function() {
     var list;
     SC.get('/tracks', {q: query, license: 'cc-by-sa'}, function(tracks) {
       console.log(tracks);
+      tracks = tracks.slice(0, 6);
       list = '<ul>';
       tracks.forEach(function(track) {
-        list += '<li class="user" data-user-id="' + track.id + '">';
+        list += '<li class="track" data-track-url="' + track.uri+ '">';
         list += track.title;
         list += " / " + track.user.username;
         list += '</li>';
@@ -24,33 +25,19 @@ $(function() {
     });
   });
 
-  // $(document).on('click', '.user', function() {
-  //   var userId = $(this).data('user-id');
-  //   var path = '/users/' + userId + '/tracks';
-  //   var html = '';
-  //   SC.get(path, function(tracks) {
-  //     console.log(tracks);
-  //     html += '<ul>';
-  //     tracks = tracks.slice(0, 11);
-  //     tracks.forEach(function(track) {
-  //       html += '<li class="track" data-track-url="'+track.uri+'">';
-  //       html += track.title;
-  //       html += '</li>';
-  //     });
-  //     html += '</ul>';
-  //     $('.tracks').html(html);
-  //   });
-  // });
+  $(document).on('click', '.user', function() {
 
-  // $(document).on('click', '.track', function() {
-  //   var trackUrl = $(this).data('track-url');
-  //   var iframe = $('.widget iframe')[0];
-  //   updateWidget(trackUrl, iframe)
-  // });
+  });
 
-  // function updateWidget(url, iframe) {
-  //   var iframeId = iframe.id;
-  //   var widget = SC.Widget(iframe);
-  //   widget.load(url);
-  // }
+  $(document).on('click', '.track', function() {
+    var trackUrl = $(this).data('track-url');
+    var iframe = $('.widget iframe')[0];
+    updateWidget(trackUrl, iframe)
+  });
+
+  function updateWidget(url, iframe) {
+    var iframeId = iframe.id;
+    var widget = SC.Widget(iframe);
+    widget.load(url);
+  }
 });
