84d4ff3c3857068cb11768e4c5d752f2006cd4ba
apradhn <abhijit.pradhan6@gmail.com>
2015-09-10 22:04:41 -0400

    scales the number returned from moving the fader from 0-100

--- public/javascripts/crossfader.js
+++ public/javascripts/crossfader.js
@@ -1,3 +1,31 @@
 $(function() {
-  $('#fader').draggable({axis: 'x', containment: ".bottom"});
+  var iframe = $('#a .widget iframe')[0]; 
+  var aWidget = SC.Widget(iframe);
+  var bWidget = SC.Widget(iframe);  
+  var center = $('#fader').position().left;
+  var divisor = center * 2;
+
+  $('#fader').draggable({axis: 'x', containment: ".bottom", drag: function(event, ui) {
+    var parentWidth = $(this).parent().width();
+    var faderWidth = $(this).width();
+    var span = parentWidth - faderWidth;
+    var offset = $(this).offset();
+    var position = $(this).position();
+    var diff = position.left - center;
+
+    console.log("span: " + span);
+    console.log("offset: " + offset.left);
+    console.log("position: " + position.left);
+    console.log(event);
+    console.log("center: " + center);
+    diff += (span / 2);
+    diff /= (span * 0.01);
+    console.log('diff: ' + diff);
+  }});
+
+  // $('#fader').click(function(event) {
+  //   console.log(event);
+  //   console.log($(this).position().left);
+  // });
 });
+
